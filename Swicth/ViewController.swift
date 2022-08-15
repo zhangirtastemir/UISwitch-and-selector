2953c2c757fa58194d17eba0494a01286aed529a
Zhangirkhan <zhangirtastemir@github.com>
2022-08-16 00:12:42 +0600

    UISwitch

--- Swicth/ViewController.swift
+++ Swicth/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let mySwitch2 = UISwitch ()
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+        self.button.frame = CGRect(x: 100, y: 200, width: 200, height: 100)
+        self.button.backgroundColor = UIColor.orange
+        self.button.setTitle("OK", for: .normal)
+        self.button.setTitle("кнопка нажата", for: .highlighted)
+        
+        self.view.addSubview(self.button)
+        self.mySwitch2.frame = CGRect.zero
+        
+        self.mySwitch2.center = self.view.center
+        self.view.addSubview(self.mySwitch2)
+        
+        //of
+        self.mySwitch2.tintColor = UIColor.green
+        
+        //рычажок
+        self.mySwitch2.thumbTintColor = UIColor.red
+        
+        //on
+        self.mySwitch2.onTintColor = UIColor.blue
+        
+        self.mySwitch2.addTarget(self, action: #selector(isOn(target:)) , for: .valueChanged)
 }
 
+    @objc func isOn(target: UISwitch) {
+        if target.isOn {
+            self.button.isUserInteractionEnabled = false
+        } else {
+            self.button.isUserInteractionEnabled = true
+        }
+        
+}
+}
